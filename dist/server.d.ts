6afb28e964953429c2aea7ae9ffccb3911c63aa4
David Melo <davidmelo@desmdesigns.com>
2023-03-29 02:58:31 -0400

    latest

--- dist/server.d.ts
+++ dist/server.d.ts
@@ -1,4 +1,3 @@
-/// <reference types="node" />
 import { schema } from './schema';
 import { CreateServerProps } from './types';
 declare const createStripeGraphQLServer: (params?: CreateServerProps) => import("http").Server<typeof import("http").IncomingMessage, typeof import("http").ServerResponse>;
